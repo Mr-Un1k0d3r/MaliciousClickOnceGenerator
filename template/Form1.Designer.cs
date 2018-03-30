49f5ed7354afac197881e981d1de3657c4d18301
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2018-03-30 17:26:07 -0400

    Create Form1.Designer.cs

---
+++ template/Form1.Designer.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace ClickOnceTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(637, 760);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("[URL]?u=" + Environment.GetEnvironmentVariable("USERNAME"), System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 760);
+            this.Controls.Add(this.webBrowser1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(655, 805);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(655, 805);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Tag = " ";
+            this.Text = "[TITLE]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
